2cca69308fe0bc9f0e2253b6fc0eaaed9afa1147
Tim Mathey <tim.mathey@gmail.com>
2024-05-17 04:02:20 +0200

    file including details on print command

---
+++ print.py
@@ -0,0 +1,13 @@
+#print modifiers
+#escape quote
+print("I said \"hello\" to him")
+#single quote inside double quote
+print("I said 'hello' to him")
+#double quote inside double quote
+print('I said "hello" to him')
+
+#add paragraph using \n
+print('I said "hello"\nbut he would not answer')
+
+#concatenate strings
+print('Hello'+" "+"Tim")
